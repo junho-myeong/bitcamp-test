ff65b0946a41ee565cc165832833a2a505dcb74e
KyoungHyunPark <findkh@nate.com>
2022-03-10 17:51:02 +0900

    test3

--- app/src/main/java/com/bitcamptest/Hello2.java
+++ app/src/main/java/com/bitcamptest/Hello2.java
@@ -4,5 +4,6 @@ public class Hello2 {
   public static void main(String[] args) {
     System.out.println("경현2");
     System.out.println("hahahahahahahahaahha");
+    System.out.printf("%s", "헐");
   }
 }
