e11dad227cff618b27cd460b81f282badfa5d932
uniquejw <jaewongogo@naver.com>
2022-03-10 19:06:58 +0900

    1

---
+++ app/src/main/java/com/bitcamptest/Hello5.java
@@ -0,0 +1,10 @@
+package com.bitcamptest;
+
+public class Hello5 {
+  public static void main(String[] args) {
+    System.out.println("굿굿");
+  }
+  static void m1() {
+    System.out.println("굿굿굿");
+  }
+}
