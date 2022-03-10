43dfebc24598a618f86d4a957eb9be52c5fb17f0
kmk3296 <kmk3296@gmail.com>
2022-03-10 18:39:09 +0900

    연습중

--- app/src/main/java/com/bitcamptest/Hello0.java
+++ app/src/main/java/com/bitcamptest/Hello0.java
@@ -4,6 +4,7 @@ public class Hello0 {
 
   public static void main(String[] args) {
     System.out.println("명준호!");
+    System.out.println("무야호~");
   }
   static void m1() {
     System.out.println("연습연습!");
