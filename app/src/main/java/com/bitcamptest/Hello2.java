53096703f3c8ab1b57d0011a2722ad9fac2a0ac2
junho-myeong <https://github.com/junho-myeong?tab=repositories>
2022-03-10 17:46:17 +0900

    이상준

--- app/src/main/java/com/bitcamptest/Hello2.java
+++ app/src/main/java/com/bitcamptest/Hello2.java
@@ -3,5 +3,9 @@ package com.bitcamptest;
 public class Hello2 {
   public static void main(String[] args) {
     System.out.println("경현2");
+    m1();
+  }
+  static void m1() {
+    System.out.println("이상준 바보입니다");
   }
 }
