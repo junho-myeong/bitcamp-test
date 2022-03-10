1a0d14018ebe98ae4b664a2b2ad3b07ab0dfff84
jys7622 <jys7622@gmail.com>
2022-03-10 17:44:11 +0900

    수정했다

---
+++ app/src/main/java/com/bitcamptest/Hello101_1.java
@@ -0,0 +1,7 @@
+package com.bitcamptest;
+
+public class Hello101_1 {
+  public static void main(String[] args) {
+    System.out.println("요섭!");
+  }
+}
---
+++ app/src/main/java/com/bitcamptest/Hello101_2.java
@@ -0,0 +1,7 @@
+package com.bitcamptest;
+
+public class Hello101_2 {
+  public static void main(String[] args) {
+    System.out.println("요섭!");
+  }
+}
