135fa3cbf4ef64464fb04e45cbd8a59036cc79bd
uniquejw <jaewongogo@naver.com>
2022-03-10 19:07:54 +0900

    1

---
+++ app/src/main/java/com/bitcamptest/Hello55.java
@@ -0,0 +1,10 @@
+package com.bitcamptest;
+
+public class Hello55 {
+  public static void main(String[] args) {
+    System.out.println("학생");
+  }
+  static void m1() {
+    System.out.println("들");
+  }
+}
