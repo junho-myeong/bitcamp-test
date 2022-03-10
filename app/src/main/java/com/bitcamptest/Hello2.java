b9aa192495a7784af1062c17aa0b2122f61157d4
junho-myeong <https://github.com/junho-myeong?tab=repositories>
2022-03-10 17:49:26 +0900

    백업3

--- app/src/main/java/com/bitcamptest/Hello2.java
+++ app/src/main/java/com/bitcamptest/Hello2.java
@@ -4,6 +4,7 @@ public class Hello2 {
   public static void main(String[] args) {
     System.out.println("경현2");
     m1();
+    System.out.println("명준호1");
   }
   static void m1() {
     System.out.println("이상준 바보입니다");
